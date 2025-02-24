241b5b525174c0c633c9f2a3eab02d6ea95aaae8
R-Mehdi94 <mehdi.bellilpro@gmail.com>
2025-02-24 20:58:23 +0100

    update familyId to chatId

--- dist/index.js
+++ dist/index.js
@@ -18,31 +18,31 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => {
         console.log(`Socket déconnecté : ${socket.id}`);
     });
-    socket.on("joinFamily", (data) => {
-        const familyId = Number(data);
-        if (!familyId) {
-            console.error("ID de la famille manquant ou invalide");
+    socket.on("joinChat", (data) => {
+        const chatId = Number(data);
+        if (!chatId) {
+            console.error("ID du chat manquant ou invalide");
             return;
         }
-        socket.join(familyId.toString());
-        console.log(`Socket ${familyId} a rejoint la famille : ${familyId}`);
+        socket.join(chatId.toString());
+        console.log(`Socket ${chatId} a rejoint le chat : ${chatId}`);
         // Vérifiez les sockets dans la room
-        const socketsInRoom = io.sockets.adapter.rooms.get(familyId.toString());
-        console.log(`Sockets dans la room ${familyId}:`, socketsInRoom ? Array.from(socketsInRoom) : []);
+        const socketsInRoom = io.sockets.adapter.rooms.get(chatId.toString());
+        console.log(`Sockets dans la room ${chatId}:`, socketsInRoom ? Array.from(socketsInRoom) : []);
         // Confirmation au client
-        socket.emit("joinedFamily", { familyId });
+        socket.emit("joinedChat", { chatId });
     });
     socket.on("sendMessage", (data) => {
         if (typeof data === 'string') {
             data = JSON.parse(data);
         }
-        const { familyId, senderId, senderNom, senderPrenom, content } = data;
-        if (!familyId || !senderId || !content || !senderNom || !senderPrenom) {
+        const { chatId, senderId, senderNom, senderPrenom, content } = data;
+        if (!chatId || !senderId || !content || !senderNom || !chatId || !senderPrenom) {
             console.error("Données de message invalides");
             return;
         }
-        console.log(`Message reçu : ${content} de ${senderId} ${senderPrenom + senderNom} pour la famille ${familyId}`);
-        socket.broadcast.to(familyId.toString()).emit("message", { senderId, senderNom, senderPrenom, content });
+        console.log(`Message reçu : ${content} de ${senderId} ${senderPrenom + senderNom} pour le chat ${chatId}`);
+        socket.broadcast.to(chatId.toString()).emit("message", { senderId, senderNom, chatId, senderPrenom, content });
     });
 });
 console.log("Serveur WebSocket en cours d'exécution...");
